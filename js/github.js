e6657c112443cd3dec92597ae84f4b1e9fab51fc
Delkox <datoguer@hotmail.com>
2016-11-03 14:21:04 -0600

    creando campos nuevos en api github
    creando los campos de busqueda en html

--- js/github.js
+++ js/github.js
@@ -1,7 +1,9 @@
 var. app = angular.module('app', []);
 app.controller('MainController', function($http){
 	var vm = this;
-    $http.get('https://api.github.com').then(function(res){
-    	console.log(res);
-    })
+    vm.fetchData = function(user){
+	    $http.get('https://api.github.com'´+ user.name).then(function(res){
+	    	console.log(res);
+	    })
+    }
 })
