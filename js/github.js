479de84d19490a1a936e8f06330d41dd53d1d514
Delkox <datoguer@hotmail.com>
2016-11-03 15:54:52 -0600

    revisando codigo

--- js/github.js
+++ js/github.js
@@ -1,11 +1,11 @@
-var. app = angular.module('app', []);
-app.controller('MainController', function($http){
+var app = angular.module('app', []);
+app.controller('MainCtrl', function($http){
 	var vm = this;
-    vm.fetchData = function(user){
-    	vm.loading = true;
-	    $http.get('https://api.github.com'´+ user.name).then(function(res){
-	    	vm.data = res;
-	    	vm.loading = false;
-	    })
-    }
+	vm.fetchData = function(user){
+		vm.loading = true;
+		$http.get('https://api.github.com/users/' + user.name).then(function(res){
+			vm.data = res;
+			vm.loading = false;
+		})
+	}
 })
