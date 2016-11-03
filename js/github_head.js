d9c5188d6a9573b92dc385b2e2e95b31e0ef44d6
Delkox <datoguer@hotmail.com>
2016-11-03 16:33:23 -0600

    optimizando

--- js/github_head.js
+++ js/github_head.js
@@ -5,6 +5,10 @@ app.controller('MainCtrl', function($http){
 		vm.loading = true;
 		$http.get('https://api.github.com/users/' + user.name).then(function(res){
 			vm.data = res;
+
+			vm.name = res.data.name || 'Not found';
+			vm.location = res.data.location || 'Not found';
+
 			vm.loading = false;
 		})
 	}
