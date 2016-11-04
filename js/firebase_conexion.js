38c32c5097eb46c03a1948d41ff59b60fe4f254c
Delkox <datoguer@hotmail.com>
2016-11-04 05:28:53 -0600

    pruebas firebase

--- js/firebase_conexion.js
+++ js/firebase_conexion.js
@@ -1,4 +1,4 @@
-var config = {
+ var config = {													//otro JSON
     apiKey: "AIzaSyAU5rM0ToeXT5u5lDsbYKqXGHvlkbjHrVM",
     authDomain: "busqueda-b35b3.firebaseapp.com",
     databaseURL: "https://busqueda-b35b3.firebaseio.com",
@@ -6,3 +6,8 @@ var config = {
     messagingSenderId: "1068056825197"
   };
   firebase.initializeApp(config);
+
+  var ref = new firebase(config.databaseURL);
+  ref.child("text").on("value", function(snapshot){
+  	console.log(snapshot.val());
+  });
