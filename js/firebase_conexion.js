6a687d5c59ab1415a61e6b719b5b0863bc028832
Delkox <datoguer@hotmail.com>
2016-11-04 12:09:51 -0600

    preparando angularFire

--- js/firebase_conexion.js
+++ js/firebase_conexion.js
@@ -1,8 +1,18 @@
-var config = {
-	    apiKey: "AIzaSyAU5rM0ToeXT5u5lDsbYKqXGHvlkbjHrVM",
-	    authDomain: "busqueda-b35b3.firebaseapp.com",
-	    databaseURL: "https://busqueda-b35b3.firebaseio.com",
-	    storageBucket: "busqueda-b35b3.appspot.com",
-	    messagingSenderId: "1068056825197"
-};
-firebase.initializeApp(config);
+{function() {
+
+	var config = {
+		    apiKey: "AIzaSyAU5rM0ToeXT5u5lDsbYKqXGHvlkbjHrVM",
+		    authDomain: "busqueda-b35b3.firebaseapp.com",
+		    databaseURL: "https://busqueda-b35b3.firebaseio.com",
+		    storageBucket: "busqueda-b35b3.appspot.com",
+		    messagingSenderId: "1068056825197"
+	};
+	firebase.initializeApp(config);
+
+	angular.module('app', ['firebase']);
+	angular.controller('MyCrtl', function($firebaseObject){
+		const rootRef = firebase.database().ref().child('angular');
+		const ref = rootRef.child('object');
+		this.object = $firebaseObject(ref);
+	});
+}());
