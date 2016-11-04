f7b4a44f81d333eb26d455f65734d50345adb70f
Delkox <datoguer@hotmail.com>
2016-11-03 22:44:30 -0600

    recuperando funciones

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+//Ocultar y mostrar
+function proceso(){
+document.getElementById('ocultar').style.display = 'none';
+document.getElementById('mostrar').style.display = 'block';}
+
+//Evento de boton para gitHub
+var boton_gitHub= document.getElementById('boton_gitHub')
+boton_gitHub.addEventListener("click", proceso);
+
+
---
+++ js/github.js
@@ -0,0 +1,15 @@
+/*Github*/
+var app = angular.module('app', []);
+app.controller('MainCtrl', function($http){
+	var vm = this;
+	vm.fetchData = function(user){
+		vm.loading = true;
+		$http.get('https://api.github.com/users/' + user.name).then(function(res){
+			vm.data = res;
+			vm.loading = false;
+		})
+	}
+})
+
+
+
