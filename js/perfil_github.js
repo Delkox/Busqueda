9bc4314b60754a34c5fc04578eb10d40599e1e35
Delkox <datoguer@hotmail.com>
2016-11-04 05:05:16 -0600

    creando un buen codigo JSON

--- js/perfil_github.js
+++ js/perfil_github.js
@@ -1,27 +1,5 @@
 /*Github user data*/
 var app = angular.module('app', []);
-
-app.controller('MainCtrl', function($http){
-	var rm = this;
-	rm.fetchData = function(user){
-		rm.loading = true;
-		$http.get('https://api.github.com/users/' + user.name + '/repos').then(function(res){
-			rm.data = res;
-			var largo = res.data.length;
-			rm.loading = false;
-			x=0;
-			var lista_repos = document.getElementById('lista_repos');
-			while(x < largo){
-				lista_repos.innerHTML+= "<li class='list-group-item'>"+ " <h4> " + res.data[x].name + "</h4>" + "</br>"+ res.data[x].description + "</li>";
-				x++;
-			}
-		})
-	}
-
-})
-
-/*Github user data
-var app = angular.module('app', []);
 var ruta_img; 
 
 app.controller('MainCtrl', function($http){
@@ -67,8 +45,20 @@ app.controller('MainCtrl', function($http){
 			usuario_ubicacion.innerHTML= "Ubicacion: " + vm.data.ubicacion;
 
 		})
+
+		$http.get('https://api.github.com/users/' + user.name + '/repos').then(function(res){
+			vm.repo = res;
+			var largo = res.data.length;
+			//vm.loading = false;
+			x=0;
+			var lista_repos = document.getElementById('lista_repos');
+			while(x < largo){
+				lista_repos.innerHTML+= "<li class='list-group-item'>"+ " <h4> " + res.data[x].name + "</h4>" + "</br>"+ res.data[x].description + "</li>";
+				x++;
+			}
+		})
 	}
 
 
-})*/
+})
 
