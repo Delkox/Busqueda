a8b9bd46f4094db6978ef1172ef0e8393c0f0104
Delkox <datoguer@hotmail.com>
2016-11-04 07:22:00 -0600

    actualizacion de un dato firebase

--- js/funciones.js
+++ js/funciones.js
@@ -6,22 +6,17 @@ var lista = document.getElementById('lista_repos');
 lista_repos.innerHTML="";
 }
 
+var db = firebase.database();
+
 function agregar(){
-	 var db = firebase.database();
 	 //Obtener informacion del comentario
 	var caja_comentario = document.getElementById('caja_comentario');
 	var comentario = caja_comentario.value;
-	  db.ref("metadata").push({
+	  db.ref("metadata").set({
 	  	pex: comentario
 	  }); 
 }
 
-function mostrar(){
-	db.ref("metadata").on('value', function(snapshot){
-		var publicar = document.getElementById("publicar");
-		publicar.innerHTML = snapshot.val();
-	});
-}
 
 //Evento de boton para gitHub
 /*var boton_gitHub = document.getElementById('boton_gitHub')
@@ -30,7 +25,13 @@ boton_gitHub.addEventListener("click", proceso);*/
 //Evento e boton para firebase
 var boton_firebase = document.getElementById('boton_firebase');
 boton_firebase.addEventListener("click", agregar);
-boton_firebase.addEventListener("click", mostrar);
+
+
+db.ref("metadata").on('value', function(snapshot){
+		var publicar = document.getElementById("publicar");
+		var a = snapshot.val();
+		publicar.innerHTML="Comentario: "+(a.pex);
+});
 
 
 
