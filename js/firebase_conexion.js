3f041238f08b049a69f11f287d39e562ea4a44b2
Delkox <datoguer@hotmail.com>
2016-11-04 13:34:50 -0600

    pruebas basicas con angular y firebase

--- js/firebase_conexion.js
+++ js/firebase_conexion.js
@@ -1,18 +1,6 @@
-(function() {
+var app = angular.module("test", []);
+app.controller("testCtrl", function($scope){
 
-	var config = {
-		    apiKey: "AIzaSyAU5rM0ToeXT5u5lDsbYKqXGHvlkbjHrVM",
-		    authDomain: "busqueda-b35b3.firebaseapp.com",
-		    databaseURL: "https://busqueda-b35b3.firebaseio.com",
-		    storageBucket: "busqueda-b35b3.appspot.com",
-		    messagingSenderId: "1068056825197"
-	};
-	firebase.initializeApp(config);
+	$scope.name="abc"
 
-	angular.module('app', ['firebase']);
-	angular.controller('MyCrtl', function($firebaseObject) {
-		const rootRef = firebase.database().ref().child('angular');
-		const ref = rootRef.child('object');
-		this.object = $firebaseObject(ref);
-	});
-}());
+})
