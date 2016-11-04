540d9554c58fa4edf2e5c4927458b4f870e22247
Delkox <datoguer@hotmail.com>
2016-11-04 06:06:18 -0600

    pruebas de firebase 3

--- js/firebase_conexion.js
+++ js/firebase_conexion.js
@@ -1,13 +1,8 @@
- var config = {													//otro JSON
-    apiKey: "AIzaSyAU5rM0ToeXT5u5lDsbYKqXGHvlkbjHrVM",
-    authDomain: "busqueda-b35b3.firebaseapp.com",
-    databaseURL: "https://busqueda-b35b3.firebaseio.com",
-    storageBucket: "busqueda-b35b3.appspot.com",
-    messagingSenderId: "1068056825197"
-  };
-  firebase.initializeApp(config);
-
-  var ref = new firebase(config.databaseURL);
-  ref.child("text").on("value", function(snapshot){
-  	console.log(snapshot.val());
-  });
+var config = {
+	    apiKey: "AIzaSyAU5rM0ToeXT5u5lDsbYKqXGHvlkbjHrVM",
+	    authDomain: "busqueda-b35b3.firebaseapp.com",
+	    databaseURL: "https://busqueda-b35b3.firebaseio.com",
+	    storageBucket: "busqueda-b35b3.appspot.com",
+	    messagingSenderId: "1068056825197"
+};
+firebase.initializeApp(config);
--- js/funciones.js
+++ js/funciones.js
@@ -6,10 +6,22 @@ var lista = document.getElementById('lista_repos');
 lista_repos.innerHTML="";
 }
 
+function agregar(){
+	 var db = firebase.database();
+	  db.ref("comentarios").set({
+	  	usuario: 
+	  });
+}
+
 //Evento de boton para gitHub
-var boton_gitHub= document.getElementById('boton_gitHub')
+var boton_gitHub = document.getElementById('boton_gitHub')
 boton_gitHub.addEventListener("click", proceso);
 
+var caja_comentario = document.getElementById('caja_comentario');
+
+//Evento e boton para firebase
+var boton_firebase = document.getElementById('boton_firebase');
+boton_firebase.addEventListener("click", agregar);
 
 
 
